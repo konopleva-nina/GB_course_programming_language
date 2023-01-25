9a2811dbadc23399f52772d1e37598a7df7414ae
Nina Konopleva <orangemise@gmail.com>
2023-01-26 00:16:05 +0300

    изменили первый пример

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введите ваше имя");
+﻿Console.WriteLine("Введите ваше имя");
 string username = Console.ReadLine();
 Console.WriteLine("Привет, ");
 Console.WriteLine(username);
